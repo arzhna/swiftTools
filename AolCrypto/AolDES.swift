33016fc1e91b84657a6fe55116847f39e94fc8c9
Arzhna Lee <arzhna@gmail.com>
2014-08-22 14:21:52 +0900

    [AolCrypto] modify AolDES Class to inherit AolSymCrypto

--- AolCrypto/AolDES.swift
+++ AolCrypto/AolDES.swift
@@ -9,123 +9,36 @@
 
 import Foundation
 
-let keyNumber = 3
-let keyLength = 8
-let initVectorLength = 56
-
-class AolDES {
-    
-    var desKey: [NSData]
-    var iVec: NSData
+class AolDES: AolSymCrypto {
     
     init() {
-        // init keys and iVector
-        self.desKey = [NSData]()
-        self.iVec = NSData()
+        // init AolSymCrypto class
+        super.init(type: .DES)
         
+        // init keys
         self.generateKeys()
     }
     
     init(key1: NSData, key2: NSData, key3: NSData) {
-        self.desKey = [NSData]()
-        self.desKey.append(key1)
-        self.desKey.append(key2)
-        self.desKey.append(key3)
+        // init AolSymCrypto class
+        super.init(type: .DES)
         
-        self.iVec = NSData()
+        // init keys
+        self.key.append(key1)
+        self.key.append(key2)
+        self.key.append(key3)
     }
     
-    deinit {
-        self.desKey.removeAll(keepCapacity: false)
+    func setNewKey(key1: NSData, key2: NSData, key3: NSData) {
+        self.key.removeAll(keepCapacity: false)
+        self.key.append(key1)
+        self.key.append(key2)
+        self.key.append(key3)
     }
     
-    
-    
-    // private methods
-    func addDummyTo8BytesAlign(data: NSData) -> NSData {
-        // calculate 8Bytes-aligned length
-        var alignedLength: Int
-        if data.length % 8 != 0 {
-            alignedLength = (data.length/8)*8 + 8
-        } else {
-            alignedLength = data.length
-        }
-        
-        // copy data to append dummy
-        var tempData = NSMutableData(data: data)
-        
-        if alignedLength > data.length {
-            // create dummy that consist of zero
-            let dummyLength = alignedLength - data.length
-            let dummyBuffer = UnsafeMutablePointer<CUnsignedChar>.alloc(dummyLength)
-            dummyBuffer.initialize(0)
-            
-            // append dummy
-            tempData.appendBytes(dummyBuffer, length: dummyLength)
-            
-            dummyBuffer.dealloc(dummyLength)
-        }
-        
-        return tempData as NSData
-    }
-    
-    func reduceDummyTo8BytesAlign(data: NSData) -> NSData {
-        var dummyLength = 0
-        let dummyBuffer = UnsafeMutablePointer<CUnsignedChar>.alloc(data.length)
-        
-        // check dummy bytes
-        data.getBytes(dummyBuffer, length: data.length)
-        for i in (data.length-8)..<data.length {
-            if dummyBuffer[i] == 0 {
-                dummyLength++
-            }
-        }
-        
-        // copy dummy-reduced data
-        var reducedData: NSData
-        if dummyLength > 0 {
-            reducedData = NSData(bytes: dummyBuffer, length: data.length-dummyLength)
-        }else{
-            reducedData = NSData(data: data)
-        }
-        
-        dummyBuffer.dealloc(data.length)
-        
-        return reducedData
-    }
-    
-    
-    
-    // public methods
-    func generateKeys(){
-        let time = Int(Double(NSDate().timeIntervalSince1970)*100000) & 0xFFFF
-        var byte = [Byte()]
-
-        srandom(UInt32(time))
-        
-        for j in 0..<keyNumber {
-            for i in 0..<keyLength {
-                byte.insert(Byte(random()%0xFF), atIndex: i)
-            }
-            self.desKey.append(NSData(bytes: byte, length: keyLength))
-            byte.removeAll(keepCapacity: false)
-        }
-    }
-    
-    func generateInitVector(){
-        let time = Int(Double(NSDate().timeIntervalSince1970)*100000) & 0xFFFF
-        var byte = [Byte()]
-
-        srandom(UInt32(time))
-        for i in 0..<initVectorLength {
-            byte.insert(Byte(random()%0xFF), atIndex: i)
-        }
-        self.iVec = NSData(bytes: byte, length: initVectorLength)
-    }
-    
-    func encrypt(plain: NSData)->NSData {
-        // 8 Bytes-align 
-        let alignedData = self.addDummyTo8BytesAlign(plain)
+    override func encrypt(plain: NSData)->NSData {
+        // add padding for 8 Bytes-align
+        let alignedData = self.addPadding(plain)
         
         // create buffer
         var encrypted = UnsafeMutablePointer<CUnsignedChar>.alloc(alignedData.length)
@@ -133,12 +46,12 @@ class AolDES {
         
         // convert to ctype from nsdata
         let plainData = UnsafeMutablePointer<CUnsignedChar>(alignedData.bytes)
-        let key1 = UnsafeMutablePointer<CUnsignedChar>(self.desKey[0].bytes)
-        let key2 = UnsafeMutablePointer<CUnsignedChar>(self.desKey[1].bytes)
-        let key3 = UnsafeMutablePointer<CUnsignedChar>(self.desKey[2].bytes)
+        let key1 = UnsafeMutablePointer<CUnsignedChar>(self.key[0].bytes)
+        let key2 = UnsafeMutablePointer<CUnsignedChar>(self.key[1].bytes)
+        let key3 = UnsafeMutablePointer<CUnsignedChar>(self.key[2].bytes)
         let iv = UnsafeMutablePointer<CUnsignedChar>(self.iVec.bytes)
         
-        des_encrypt(key1, key2, key3, iv, plainData, CInt(alignedData.length), encrypted)
+        des_encrypt(key1, key2, key3, iv, plainData, CUnsignedInt(alignedData.length), encrypted)
         
         // convert encrypted data to nsdata from ctype
         let result = NSData(bytes: encrypted, length: alignedData.length)
@@ -149,9 +62,9 @@ class AolDES {
         return result
     }
     
-    func decrypt(cipher: NSData)->NSData {
+    override func decrypt(cipher: NSData)->NSData {
         // Perhaps, cipher is already 8 bytes-aligned. But, do it one more.
-        let alignedData = self.addDummyTo8BytesAlign(cipher)
+        let alignedData = self.addPadding(cipher)
         
         // create buffer
         var decrypted = UnsafeMutablePointer<CUnsignedChar>.alloc(alignedData.length)
@@ -159,24 +72,23 @@ class AolDES {
         
         // convert to ctype from nsdata
         let cipherData = UnsafeMutablePointer<CUnsignedChar>(alignedData.bytes)
-        let key1 = UnsafeMutablePointer<CUnsignedChar>(self.desKey[0].bytes)
-        let key2 = UnsafeMutablePointer<CUnsignedChar>(self.desKey[1].bytes)
-        let key3 = UnsafeMutablePointer<CUnsignedChar>(self.desKey[2].bytes)
+        let key1 = UnsafeMutablePointer<CUnsignedChar>(self.key[0].bytes)
+        let key2 = UnsafeMutablePointer<CUnsignedChar>(self.key[1].bytes)
+        let key3 = UnsafeMutablePointer<CUnsignedChar>(self.key[2].bytes)
         let iv = UnsafeMutablePointer<CUnsignedChar>(self.iVec.bytes)
-        
+
         // decrypt
-        des_decrypt(key1, key2, key3, iv, cipherData, CInt(alignedData.length), decrypted)
+        des_decrypt(key1, key2, key3, iv, cipherData, CUnsignedInt(alignedData.length), decrypted)
         
         // convert decrypted data to nsdata from ctype
         var result = NSData(bytes: decrypted, length: alignedData.length)
         
-        // reduce dummy data
-        result = reduceDummyTo8BytesAlign(result)
+        // reduce padding
+        result = self.reducePadding(result)
         
         //dealloc buffer
         decrypted.dealloc(alignedData.length)
         
         return result
-
     }
 }
